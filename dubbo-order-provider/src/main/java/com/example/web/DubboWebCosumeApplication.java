f023eac3037575c5df6dcf57364fe42d191e928c
liusk <lsk@servyou.com.cn>
2022-06-22 15:03:39 +0800

    init

---
+++ dubbo-order-provider/src/main/java/com/example/web/DubboWebCosumeApplication.java
@@ -0,0 +1,13 @@
+package com.example.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DubboWebCosumeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DubboWebCosumeApplication.class, args);
+    }
+
+}
